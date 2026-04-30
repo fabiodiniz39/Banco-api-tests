2a8d9192aba6982d40d89cb72ce6aec451a513de
fabio <fabio.dab@hotmail.com>
2026-04-30 13:24:44 -0300

    Criei um helper de autenticação para obter o token

--- test/login.test.js
+++ test/login.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
 const { expect } = require('chai')
+require('dotenv').config()
 
 describe('login', () => {
     describe('POST /login', () => {
         it('deve retornar 200 com um token em string quando usar credenciais válidas', async () => {
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-type', 'application/json')
                 .send({
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,20 +1,14 @@
 const request = require('supertest');
 const {expect} = require('chai');
+require('dotenv').config()
+const { obterToken } = require('../helpers/autenticacao');
 
 describe('transferencias', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
-                .post('/login')
-                .set('Content-type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                })                       
-            
-            const token = respostaLogin.body.token
+            const token = await obterToken('julio.lima', '123456')
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
               .post('/transferencias')
               .set('content-type', 'application/json')
               .set('authorization', `Bearer ${token}`)
@@ -29,15 +23,7 @@ describe('transferencias', () => {
         })
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
-                .post('/login')
-                .set('Content-type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                })                       
-            
-            const token = respostaLogin.body.token
+            const token = await obterToken('julio.lima', '123456')
 
             const resposta = await request('http://localhost:3000')
               .post('/transferencias')
